0653bd9dfbd53343c50837bdc5d3dd6809f192ce
bootstraponline <code@bootstraponline.com>
2015-06-06 12:48:06 -0400

    Release 0.0.2

--- lib/sauce_platforms/version.rb
+++ lib/sauce_platforms/version.rb
@@ -1,4 +1,4 @@
 module Platforms
-  VERSION = '0.0.1' unless defined? ::Platforms::VERSION
-  DATE    = '2015-05-12' unless defined? ::Platforms::DATE
+  VERSION = '0.0.2' unless defined? ::Platforms::VERSION
+  DATE    = '2015-06-06' unless defined? ::Platforms::DATE
 end
