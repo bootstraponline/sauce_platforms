80733a0576f612edd432260fc23ddb43edaf097c
bootstraponline <code@bootstraponline.com>
2015-07-17 18:02:25 -0400

    Fix Android platform support

--- lib/sauce_platforms/generate_sauce_platforms.rb
+++ lib/sauce_platforms/generate_sauce_platforms.rb
@@ -33,9 +33,21 @@ platforms.each do |platform|
   browser = platform['api_name']
   version = platform['short_version']
 
-  operating_systems[os]          ||= {}
-  operating_systems[os][browser] ||= []
-  operating_systems[os][browser] << version
+  operating_systems[os] ||= {}
+
+  # android has 'device' value which is sent in deviceName desired cap.
+  device                = platform['device']
+  if os == 'Linux' && browser == 'android'
+    os                                     = 'android'
+    operating_systems[os]                  ||= {}
+    operating_systems[os][browser]         ||= {}
+    operating_systems[os][browser][device] ||= []
+    operating_systems[os][browser][device] << version
+  else
+    operating_systems[os][browser] ||= []
+    operating_systems[os][browser] << version
+  end
+
 end
 
 # platforms.length
@@ -91,43 +103,62 @@ operating_systems.each do |os, browser_hash|
 
   # now build version specific files
   browser_hash.each do |browser, version_array|
-    filecase_browser    = filecase(browser)
-    os_browser_filename = "#{os_file_name}_#{filecase_browser}"
-    File.open(File.join(os_dir, "#{os_browser_filename}.rb"), 'w') do |file|
-      os_browser_class_name = "#{os_class_name}_#{filecase_browser}"
-      file.puts "module #{module_name}"
-      file.puts "  module #{os_browser_class_name}"
-      file.puts '    class << self'
-      file.puts "      def #{os_browser_filename} version_string"
-      file.puts "        ['#{os}', '#{browser}', version_string.to_s]"
-      file.puts '      end'
-      file.puts
-      file.puts '      def v version_string'
-      file.puts "        #{os_browser_filename} version_string"
-      file.puts '      end'
-      file.puts
+    browser_version_pairs = [[browser, version_array]]
 
-      methods << "#{module_name}.#{windows_alias(os_file_name)}.#{filecase_browser}.v'123'"
+    is_android            = browser == 'android'
+    browser_version_pairs = version_array if is_android
 
-      # android has different versions based on deviceName so we must
-      # take the unique versions otherwise duplicate methods will be created
-      version_array.uniq.each do |version|
-        filecase_version = filecase(version)
-        # prefix with v if version starts with a number
-        # 'dev' and 'beta' don't need to be prefixed
-        method_name      = version.match(/^\d/) ? "v#{filecase_version}" : filecase_version
+    browser_version_pairs.each do |browser, version_array|
+      filecase_browser    = filecase(browser)
+      os_browser_filename = "#{os_file_name}_#{filecase_browser}"
+      File.open(File.join(os_dir, "#{os_browser_filename}.rb"), 'w') do |file|
+        os_browser_class_name = "#{os_class_name}_#{filecase_browser}"
+        file.puts "module #{module_name}"
+        file.puts "  module #{os_browser_class_name}"
+        file.puts '    class << self'
+        file.puts "      def #{os_browser_filename} version_string"
+        if is_android
+          # android must specify a device name
+          device_name  = browser
+          # os for Android is always linux
+          # browser name for Android is always android
+          browser = 'android'
+          os      = 'Linux'
+          # desktop_os, browser_name, android_version, device_name
+          file.puts "        ['#{os}', '#{browser}', version_string.to_s, deviceName: '#{device_name}']"
+        else
 
-        file.puts "      def #{method_name}"
-        file.puts "        #{os_browser_filename} '#{version}'"
+          file.puts "        ['#{os}', '#{browser}', version_string.to_s]"
+        end
+        file.puts '      end'
+        file.puts
+        file.puts '      def v version_string'
+        file.puts "        #{os_browser_filename} version_string"
         file.puts '      end'
         file.puts
 
-        methods << "#{module_name}.#{windows_alias(os_file_name)}.#{filecase_browser}.#{method_name}"
-      end
+        methods << "#{module_name}.#{windows_alias(os_file_name)}.#{filecase_browser}.v'123'"
 
-      file.puts '    end'
-      file.puts '  end'
-      file.puts 'end'
+        # android has different versions based on deviceName so we must
+        # take the unique versions otherwise duplicate methods will be created
+        version_array.uniq.each do |version|
+          filecase_version = filecase(version)
+          # prefix with v if version starts with a number
+          # 'dev' and 'beta' don't need to be prefixed
+          method_name      = version.match(/^\d/) ? "v#{filecase_version}" : filecase_version
+
+          file.puts "      def #{method_name}"
+          file.puts "        #{os_browser_filename} '#{version}'"
+          file.puts '      end'
+          file.puts
+
+          methods << "#{module_name}.#{windows_alias(os_file_name)}.#{filecase_browser}.#{method_name}"
+        end
+
+        file.puts '    end'
+        file.puts '  end'
+        file.puts 'end'
+      end
     end
   end
 
